0ecc1344856f5b0bf7b7e7a73beac78ca6e849b0
John Blischak <jdblischak@gmail.com>
2021-01-05 13:31:29 -0500

    More tests for faviconIco().

--- inst/tinytest/test-faviconIco.R
+++ inst/tinytest/test-faviconIco.R
@@ -21,4 +21,20 @@ if (at_home()) {
     faviconPlease("https://www.phosphosite.org/"),
     "https://www.phosphosite.org/favicon.ico"
   )
+
+  expect_identical_xl(
+    faviconIco("http", "www.informatics.jax.org", "/searchtool/Search.do?query="),
+    "http://www.informatics.jax.org/favicon.ico"
+  )
+
+  expect_identical_xl(
+    faviconIco("https", "www.ncbi.nlm.nih.gov",
+               "/CCDS/CcdsBrowse.cgi?REQUEST=CCDS&ORGANISM=9606&BUILDS=CURRENTBUILDS&DATA="),
+    "https://www.ncbi.nlm.nih.gov/favicon.ico"
+  )
+
+  expect_identical_xl(
+    faviconIco("https", "www.uniprot.org", "/uniprot/"),
+    "https://www.uniprot.org/favicon.ico"
+  )
 }
